73aae276ce3b30b224c6ca0033a0e1edf00ceed2
Karn12003 <nawakarnmek@gmail.com>
2021-11-04 23:45:06 +0700

    add ui login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,26 @@
 import React, { ReactElement } from "react";
 import { ReactComponent as SpaceCat } from "../assets/images/space-cat.svg";
+import { ReactComponent as Footerpageone } from "../assets/images/footerpageone.svg";
+import { ReactComponent as Stars } from "../assets/images/stars.svg";
 
 interface Props {}
 
 export default function Login({}: Props): ReactElement {
   return (
-    <div className="bg-red-900">
-      Login
-      <SpaceCat />
+    <div className="bg-gradient-to-b from-forthpurple to-fifthpurple h-screen w-screen font-poppins flex">
+      <Stars className="absolute h-full w-full z-0" />
+      {/* <BgLogin className=" object-cover overflow-hidden object-bottom"> */}
+      <div className="max-w-lg m-auto bg-white py-10  rounded-3xl px-20 z-10">
+        <p className="text-firstpurple text-center text-2xl">Sign In</p>
+        <div className="flex mt-10">
+          <input
+            className="border-secondpurple border-2 rounded-full p-2"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+      {/* </BgLogin> */}
+      <Footerpageone className="absolute bottom-0 object-cover" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      firstpurple: "#9F85FB",
+      secondpurple: "#BCA4FF",
+      thirdpurple: "#C4CAFF",
+      forthpurple: "#211342",
+      fifthpurple: "#7A65B7",
+      transparent: "transparent",
+
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
+    extend: {
+      backgroundImage: {
+        bglogin: "url('./assets/images/Login.svg')",
+      },
+    },
   },
+
   variants: {
     extend: {},
   },
