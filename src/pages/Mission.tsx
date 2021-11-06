4dad92e58852a558776021119da47824dee770de
Techin Techintananan <senmee99@hotmail.com>
2021-11-07 00:14:57 +0700

    Finish Mission.tsx

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -4,14 +4,14 @@ import planet from "../assets/images/Scoreboard/planet.png";
 import missionHeader from "../assets/images/Scoreboard/missionHeader.png";
 import { ReactComponent as Planet } from "../assets/images/Scoreboard/planet.svg";
 import { ReactComponent as MissionHeader } from "../assets/images/Scoreboard/missionHeader.svg";
-import { ReactComponent as Footer } from "../assets/images/footer.svg";
+import footer from "../assets/images/footer.png";
 import game1 from "../assets/images/Scoreboard/game1.png";
 import game2 from "../assets/images/Scoreboard/game2.png";
 import game3 from "../assets/images/Scoreboard/game3.png";
 import game4 from "../assets/images/Scoreboard/game4.png";
 import game5 from "../assets/images/Scoreboard/game5.png";
 import game6 from "../assets/images/Scoreboard/game6.png";
-import lbBack from "../assets/images/Scoreboard/leaderBoardBackground.png";
+import trophy from "../assets/images/Scoreboard/trophy.png";
 
 interface Props {}
 
@@ -21,54 +21,61 @@ export default function Scoreboard({}: Props): ReactElement {
       <Stars className="absolute h-full w-full z-0" />
 
       <Planet className="absolute top-0 right-0" />
-      <div className="w-full">
+      <div className="w-full m-6">
         <div>
           <MissionHeader className="m-auto" />
         </div>
-        <div className="grid grid-rows-2 grid-cols-12 w-fulls m-6">
+        <div className="grid grid-rows-2 grid-cols-12 w-fulls m-3">
           <div className="p-3 col-span-3 row-span-2 gap-3">
-            {/* <img src={lbBack} className="object-contain m-auto" /> */}
-            <div className="bg-gradient-to-b from-lbFirstpurple to-fifthpurple rounded-3xl w-3/4 h-3/4 m-auto mt-3 text-center shadow-2xl py-6">
-              <div className="bg-lbSecondpurple rounded-3xl w-5/6 h-5/6 m-auto text-center shadow-2xl py-4">
-                <div className="container w-full h-full text-center">
-                  <table className="text-left text-center bg-red-100 w-full h-full flex flex-col">
+            <div className="relative bg-gradient-to-b from-lbFirstpurple to-fifthpurple rounded-3xl w-3/4 h-3/4 m-auto mt-3 text-center shadow-2xl py-8">
+              {/* <img src={trophy} className="absolute -inset-y-40 inset-x-8" /> */}
+              <img src={trophy} className="absolute -inset-y-40 inset-x-8" />
+              <div className="bg-lbSecondpurple rounded-3xl w-5/6 h-full m-auto text-center shadow-2xl py-4">
+                <div className="w-full h-full text-center">
+                  <table className="text-left text-center w-full h-full flex flex-col">
                     <thead className="bg-black flex text-white w-full">
-                      <tr className="flex w-full">
+                      <tr className="flex w-full mr-4">
                         <th className="p-4 w-1/3">Player</th>
-                        <th className="p-4 w-1/3">Completed</th>
+                        <th className="p-4 w-1/3">Finish</th>
                         <th className="p-4 w-1/3">Time</th>
                       </tr>
                     </thead>
-                    <tbody className="bg-grey-light flex flex-col items-center overflow-y-scroll w-full z-10">
+                    <tbody className="flex flex-col items-center overflow-y-scroll w-full z-10 font-poppins text-white">
                       <tr className="flex w-full">
-                        <td className="p-4 w-1/3">Player1</td>
-                        <td className="p-4 w-1/3">5</td>
-                        <td className="p-4 w-1/3">01:05:00</td>
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
                       </tr>
                       <tr className="flex w-full">
-                        <td className="p-4 w-1/3">Player1</td>
-                        <td className="p-4 w-1/3">5</td>
-                        <td className="p-4 w-1/3">01:05:00</td>
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
                       </tr>
                       <tr className="flex w-full">
-                        <td className="p-4 w-1/3">Player1</td>
-                        <td className="p-4 w-1/3">5</td>
-                        <td className="p-4 w-1/3">01:05:00</td>
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
                       </tr>
                       <tr className="flex w-full">
-                        <td className="p-4 w-1/3">Player1</td>
-                        <td className="p-4 w-1/3">5</td>
-                        <td className="p-4 w-1/3">01:05:00</td>
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
+                      </tr>
+
+                      <tr className="flex w-full">
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
                       </tr>
                       <tr className="flex w-full">
-                        <td className="p-4 w-1/3">Player1</td>
-                        <td className="p-4 w-1/3">5</td>
-                        <td className="p-4 w-1/3">01:05:00</td>
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
                       </tr>
                       <tr className="flex w-full">
-                        <td className="p-4 w-1/3">Player1</td>
-                        <td className="p-4 w-1/3">5</td>
-                        <td className="p-4 w-1/3">01:05:00</td>
+                        <td className="p-2 w-1/3">Player1</td>
+                        <td className="p-2 w-1/3">5</td>
+                        <td className="p-2 w-1/3">01:05:00</td>
                       </tr>
                     </tbody>
                   </table>
@@ -106,7 +113,7 @@ export default function Scoreboard({}: Props): ReactElement {
       </div>
       {/* <img src={planet} className="absolute top-0 right-0" />
       <img src={missionHeader} className="flex items-center" /> */}
-      <Footer className="absolute bottom-0" />
+      <img className="h-auto w-full absolute bottom-0 " src={footer} />
     </div>
   );
 }
