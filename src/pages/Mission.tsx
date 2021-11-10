05f516caf0fa58947d2ef32c1eac97c8edc0b5a0
Karn12003 <nawakarnmek@gmail.com>
2021-11-11 05:03:49 +0700

    fixed mission

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -54,7 +54,7 @@ export default function Scoreboard({ user }: Props): ReactElement {
               <img
                 draggable={false}
                 src={trophy}
-                className="absolute -inset-y-40 inset-x-8"
+                className="absolute -inset-y-40 inset-x-5"
               />
               <div className="bg-lbSecondpurple rounded-3xl w-5/6 h-full m-auto text-center shadow-2xl py-4">
                 <div className="w-full h-full text-center">
