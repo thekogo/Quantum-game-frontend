2a3ff860e6bc91a6b404aaee116b18547b696252
Techin Techintananan <senmee99@hotmail.com>
2021-11-11 03:13:30 +0700

    Add question4.ts

--- src/constrants/question4.ts
+++ src/constrants/question4.ts
@@ -1,14 +1,98 @@
 export const questionList = [
   [
-    {
+    { // Person 1
       name: "Alan",
       carrer: "วิศวกรรมเครื่องกล",
       duty: "ทำระบบหล่อเย็นในโรงงาน",
     },
-    {
-      name: "Alan",
-      carrer: "วิศวกรรมเครื่องกล",
+    { // Person 2
+      name: "Gus",
+      carrer: "วิศวกรรมไฟฟ้า",
+      duty: "ทำงานเกี่ยวกับอุปกรณ์รับส่งคลื่นแม่เหล็กไฟฟ้า",
+    },
+    { // Person 3
+      name: "Finley",
+      carrer: "ฟิสิกส์",
+      duty: "เป็นเน็ตไอดอล",
+    },
+    { // Person 4
+      name: "Belinda",
+      carrer: "คณิตศาสตร์",
+      duty: "เป็นอาจารย์สอนคณิตศาสตร์ในมหาวิทยาลัย",
+    },
+    { // Person 5
+      name: "Ivan",
+      carrer: "วิทยาศาสตร์คอมพิวเตอร์และคณิตศาสตร์",
+      duty: "พัฒนาอัลกอริทึมและซอฟต์แวร์เพื่อแก้ปัญหาให้ลูกค้า",
+    },
+    { // Person6
+      name: "Hagan",
+      carrer: "วิศวกรรมอิเล็กทรอนิกส์และฟิสิกส์",
+      duty: "มีส่วนร่วมออกแบบ micro transistor ให้ intel",
+    },
+  ],
+  [
+    { // Person 1
+      name: "Cayla",
+      carrer: "information technologyinformation technology",
       duty: "ทำระบบหล่อเย็นในโรงงาน",
+    },
+    { // Person 2
+      name: "Dion",
+      carrer: "วิศวกรรมอิเล็กทรอนิกส์",
+      duty: "ออกแบบวงจรเฉพาะทางลงไปในชิป",
+    },
+    { // Person 3
+      name: "Emerson",
+      carrer: "วิศวกรรมคอมพิวเตอร์",
+      duty: "ออกแบบสถาปัตยกรรมคอมพิวเตอร์",
+    },
+    { // Person 4
+      name: "Hagan",
+      carrer: "วิศวกรรมอิเล็กทรอนิกส์และฟิสิกส์",
+      duty: "มีส่วนร่วมออกแบบ micro transistor ให้ intel",
+    },
+    { // Person 5
+      name: "Ivan",
+      carrer: "วิทยาศาสตร์คอมพิวเตอร์และคณิตศาสตร์",
+      duty: "พัฒนาอัลกอริทึมและซอฟต์แวร์เพื่อแก้ปัญหาให้ลูกค้า",
+    },
+    { // Person6
+      name: "Belinda",
+      carrer: "คณิตศาสตร์",
+      duty: "เป็นอาจารย์สอนคณิตศาสตร์ในมหาวิทยาลัย",
+    },
+  ],
+  [
+    { // Person 1
+      name: "Belinda",
+      carrer: "คณิตศาสตร์",
+      duty: "เป็นอาจารย์สอนคณิตศาสตร์ในมหาวิทยาลัย",
+    },
+    { // Person 2
+      name: "Dion",
+      carrer: "วิศวกรรมอิเล็กทรอนิกส์",
+      duty: "ออกแบบวงจรเฉพาะทางลงไปในชิป",
+    },
+    { // Person 3
+      name: "Emerson",
+      carrer: "วิศวกรรมคอมพิวเตอร์",
+      duty: "ออกแบบสถาปัตยกรรมคอมพิวเตอร์",
+    },
+    { // Person 4
+      name: "Finley",
+      carrer: "ฟิสิกส์",
+      duty: "เป็นเน็ตไอดอล",
+    },
+    { // Person 5
+      name: "Gus",
+      carrer: "วิศวกรรมไฟฟ้า",
+      duty: "ทำงานเกี่ยวกับอุปกรณ์รับส่งคลื่นแม่เหล็กไฟฟ้า",
+    },
+    { // Person6
+      name: "Hagan",
+      carrer: "วิศวกรรมอิเล็กทรอนิกส์และฟิสิกส์",
+      duty: "มีส่วนร่วมออกแบบ micro transistor ให้ intel",
     },
   ],
 ];
