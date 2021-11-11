229130df513815d98709115ed20279d67be3ff35
thekogo <thekogo@gmail.com>
2021-11-12 03:40:55 +0700

    change default to server

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL:
-    "http://localhost:8000/api/" ||
-    "http://qx-nstfair.pj.innosoft.kmutt.ac.th/api/",
+  baseURL: "http://qx-nstfair.pj.innosoft.kmutt.ac.th/api/",
 });
 
 export default client;
