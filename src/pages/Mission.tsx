0f63371e82781551b528fc4264658be68de775fc
Techin Techintananan <senmee99@hotmail.com>
2021-11-13 09:03:46 +0700

    Responsive Scoreboard

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -49,15 +49,15 @@ export default function Scoreboard({ user }: Props): ReactElement {
     <div className="bg-gradient-to-b from-forthpurple to-fifthpurple h-screen w-screen font-thaifonts flex overflow-hidden">
       <Stars className="absolute h-full w-full z-0" />
 
-      <Planet className="absolute z-0 top-0 right-0 transform origin-top-right md:scale-90 3xl:scale-100" />
+      <Planet className="absolute z-0 top-0 right-0 transform origin-top-right md:scale-75 xl:scale-90 3xl:scale-100" />
       <div className="w-full m-6 z-10">
         <div className=" 3xl:mb-12">
           <img draggable={false} className="mx-auto" src={missionHeader} />
         </div>
         <div className="grid grid-rows-2 grid-cols-12 w-full h-screen m-3">
-          <div className="p-3 col-span-3 row-span-2 gap-3 -mt-40 h-3/5">
-            <div className="relative  mx-auto top-10">
-              <img draggable={false} src={trophy} className="m-auto" />
+          <div className="p-3 md:col-span-4 xl:col-span-3 row-span-2 gap-3 -mt-40 h-3/5">
+            <div className="relative  mx-auto md:top-14 xl:top-10">
+              <img draggable={false} src={trophy} className="m-auto transform md:scale-75 xl:scale-100" />
             </div>
             <div className="bg-gradient-to-b from-lbFirstpurple to-fifthpurple rounded-3xl w-full h-full m-auto mt-3 text-center shadow-2xl align-middle flex">
               <div className="bg-lbSecondpurple rounded-3xl w-5/6 h-5/6 m-auto text-center shadow-2xl ">
@@ -65,9 +65,9 @@ export default function Scoreboard({ user }: Props): ReactElement {
                   <table className="table-fixed text-center w-full h-full flex flex-col">
                     <thead className="bg-lbFirstpurple flex text-white w-full mx-auto rounded-sm">
                       <tr className="flex w-full mr-4">
-                        <th className="p-4 w-2/4 ml-4">Team</th>
+                        <th className="p-4 w-2/4 ml-4 text-left">Team</th>
                         <th className="p-4 w-1/4">Finish</th>
-                        <th className="p-4 w-1/4 mr-4">Time</th>
+                        <th className="p-4 w-1/4 md:mr-6 xl:mr-0">Time</th>
                       </tr>
                     </thead>
                     <tbody className="flex flex-col items-center overflow-y-scroll overflow-x-hidd w-full h-full z-10 font-poppins text-white">
@@ -79,7 +79,7 @@ export default function Scoreboard({ user }: Props): ReactElement {
                           <td className="p-2 w-1/4 border-collapse border-b border-secondpurple">
                             {score.missionPass}
                           </td>
-                          <td className="p-2 w-1/4 border-collapse border-b mr-3 border-secondpurple">
+                          <td className="p-2 w-1/4 border-collapse border-b md:mr-6 xl:mr-3 border-secondpurple">
                             {score.durationStr}
                           </td>
                         </tr>
@@ -103,7 +103,7 @@ export default function Scoreboard({ user }: Props): ReactElement {
               </div>
             </div>
           </div>
-          <div className="row-span-1 col-span-7 w-full md:ml-4 3xl:ml-auto">
+          <div className="row-span-1 col-span-7 w-full md:ml-4 3xl:ml-auto md:mt-8 xl:mt-0">
             <div className="flex flex-row gap-x-4">
               <div className="w-1/3 transform transition duration-500 hover:scale-125 hover:-rotate-12">
                 <Link to="/mission/1">
